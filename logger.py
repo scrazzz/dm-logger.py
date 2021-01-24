626b0dd9009a550ff894c2c5207f8c2a5cd713c8
Arthaxeres <70033559+scrazzz@users.noreply.github.com>
2021-01-24 21:30:58 +0300

    v0.4.0
    Log images and gifs too.

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ A python script that logs Discord DMs on a user account.
 
 Author: scrazzz
 License: MIT
-Version: v0.3.1
+Version: v0.4.0
 """
 
 import dhooks
@@ -15,6 +15,26 @@ bot = commands.Bot(command_prefix='!', self_bot=True)
 # Make a webhook in a server where you want to log. Replace with your webhook url.
 log = dhooks.Webhook("URL")
 
+# to log images and gifs ONLY.
+async def upload_image(img):
+    async with aiohttp.ClientSession() as sess:
+        data = {
+            'key': '232565fc1a4f0d24578d9aeadc0b43ab', # you can use this or get your own api key from https://api.imgbb.com
+            'image': img
+        }
+        async with sess.post("https://api.imgbb.com/1/upload", data=data) as post:
+            js = await post.json()
+            
+            try:
+                url = js['data']['url']
+                return url
+                
+            # sometimes, very rarely it just wont upload
+            # so we ignore that.
+            # But make sure the API key is working or not.
+            except:
+                pass
+            
 @bot.event
 async def on_message(m):
     # ignore messages in servers.
@@ -29,13 +49,9 @@ async def on_message(m):
         embed.description = m.content
         
         # we also need to log attachments.
-        
-        # using proxy_url will save it for some time
-        # before it being deleted permanently.
-        # a solution would be to upload the attachment to
-        # a file upload-able website like imgur.
         for attach in m.attachments:
-            embed.add_field(name="\u200b", value=attach.proxy_url, inline=False)
+            url = await upload_image(attach.proxy_url)
+            embed.add_field(name="\u200b", value=url, inline=False)
                 
         log.send(embed=embed)
     
